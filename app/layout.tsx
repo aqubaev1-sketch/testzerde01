99d83013f062e17c8db731f0b0267477a85f907b
aisultan <a.qubaev1@gmail.com>
2026-09-25 15:12:10 +0500

    fix type mismatch in serializ00000Attempt

--- app/layout.tsx
+++ app/layout.tsx
@@ -38,7 +38,7 @@ export default function RootLayout({
         <NuqsAdapter>
           <ThemeProvider
             attribute="class"
-            defaultTheme="system"
+            defaultTheme="light"
             enableSystem
             disableTransitionOnChange
           >
